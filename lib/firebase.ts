ee1db06128190dc327f7a8d5268aa49ef777e7df
beastmodeacademic-lgtm <beast.mode.academic@gmail.com>
2026-09-03 15:25:55 +0300

    Create firebase.ts

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqHgSoxWbnzbD0NGoTawpgn4poX-JquUg",
+  authDomain: "bmacacademic-mobil.firebaseapp.com",
+  projectId: "bmacacademic-mobil",
+  storageBucket: "bmacacademic-mobil.firebasestorage.app",
+  messagingSenderId: "826324524600",
+  appId: "1:826324524600:web:4fef26ffc1200f938790cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
